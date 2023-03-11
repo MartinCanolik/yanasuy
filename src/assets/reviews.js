31a9b7e92d53264e27b31ac9ad37a27c6b021579
MartinCanolik <martin.canolik@gmail.com>
2023-03-11 10:53:18 -0300

    reviws carousel done.

--- src/assets/reviews.js
+++ src/assets/reviews.js
@@ -1,42 +1,122 @@
+const logoBooking =
+	"https://res.cloudinary.com/drhj3sc2o/image/upload/v1678401479/bookingcom-1_br2vnl.svg";
+const logoGoogle =
+	"https://res.cloudinary.com/drhj3sc2o/image/upload/v1678472737/google-maps-2020_kcnwqz.jpg";
+
 export const Reviews = [
 	{
 		name: "Romina",
-		company: "booking",
+		logo: logoBooking,
 		rating: "10",
+		stars: null,
 		review:
 			'Una paz increible y una belleza unica. Si tengo que comparalo con "algo", seria con la receta de la abuela que mas me gusta. Volveria siempre.',
 	},
 	{
 		name: "Maria",
-		company: "booking",
+		logo: logoBooking,
 		rating: "9",
+		stars: null,
 		review:
 			"Impecable y muy cálida la atención de Marcela. Hermosa la propiedad y las vistas de las sierras y del valle. Los chicos disfrutaron muchísimo de la pileta y de la mesa de ping-pong. Muy tranquilo.",
 	},
 	{
 		name: "Ricardo",
-		company: "booking",
+		logo: logoBooking,
 		rating: "9",
+		stars: null,
 		review: "Excelente lugar !!!!!",
 	},
 	{
 		name: "andreagabriela1978 castex",
-		company: "google",
-		rating: "5",
+		logo: logoGoogle,
+		rating: null,
+		stars: (
+			<div className='flex'>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+			</div>
+		),
 		review:
 			"Un lugar soñado! La atención de Marcela y su familia es espectacular! Muy cordiales y bien predispuestos! Las cabañas son hermosas y súper cómodas, la pileta y el jacuzzi espectaculares!! Para volver a visitar sin dudarlo!!",
 	},
 	{
 		name: "Lía Bedatou",
-		company: "google",
-		rating: "5",
+		logo: logoGoogle,
+		rating: null,
+		stars: (
+			<div className='flex'>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+			</div>
+		),
 		review:
 			"Hermoso lugar! Súper recomendable! Las cabañas completas y muy cómodas. La atención de Marcela y Laurita: un lujo! Nos atendieron con mucha amabilidad y cariño, incluso a Lupita ( nuestra mascota). Un lugar para disfrutar a full la belleza y la naturaleza de Merlo. Volveremos seguramente!",
 	},
 	{
 		name: "Nicolas Alejandro Fringes",
-		company: "google",
-		rating: "5",
+		logo: logoGoogle,
+		rating: null,
+		stars: (
+			<div className='flex'>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+				<img
+					src='https://res.cloudinary.com/drhj3sc2o/image/upload/v1678474301/descarga_1_jwuvof.jpg'
+					alt='star'
+				/>
+			</div>
+		),
 		review:
 			"Un lugar encantador, la vista es hermosa, las cabañas un espectáculo, y los dueños excelentes y muy atentos. Totalmente recomendable.",
 	},
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,33 +1,78 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Reviews } from "../../assets/reviews";
-
-const formatedReviews = [];
+// import { Reviews } from "../../assets/reviews";
 const logoBooking =
 	"https://res.cloudinary.com/drhj3sc2o/image/upload/v1678401479/bookingcom-1_br2vnl.svg";
-const logoGoogle = "";
+const logoGoogle =
+	"https://documents.reverso.net/img/new_design/icons/ic-google.svg";
+const estrellas = "https://www.finistarmor.com/images/five-stars.png";
+// const formatedReviews = [];
 
-// for (let i = 0; i > Reviews / 3; i++) {
+// const handleFormatReviews = () => {
+// 	for (let i = 0; i < Reviews.length / 3; i++) {
+// 		const groupedReviews = `<div className='w-1/3 border-2 border-white p-3 px-3'>
+// 		<img
+// 		src=${Reviews[i].logo}
+// 			alt='logo'
+// 			className='h-20 opacity-70 my-3'
+// 		/>
+// 		<h2 className='text-white text-xl my-3'>${Reviews[i].name}</h2>
+// 		<p className='text-xm text-white my-3 text-center'>
+// 		${Reviews[i].review}
+// 		</p>
+// 		<h2 className='text-white text-3xl my-3'>${Reviews[i].rating}</h2>
+// 		<div>${Reviews[i].stars}</div>
 
-// 	const groupedReviews = `<div className='w-1/3 border-2 border-white p-3 px-3'>
-// 	<img
-// 	src=${Reviews?[i] === 'booking'? logoBooking : logoGoogle}
-// 		alt='logo'
-// 		className='h-20 opacity-70 my-3'
-// 	/>
-// 	<h2 className='text-white text-xl my-3'>Mirta: </h2>
-// 	<p className='text-xm text-white my-3 text-center'>
-// 		"tengo una muy muy buena critica para hacer porque me parece q
-// 		el complejo simplemente se va al remil pasto mi reynaldo raul
-// 		altas vacas pegamos aca con los pibes, epicardo."
-// 	</p>
-// 	<h2 className='text-white text-3xl my-3'>10</h2>
+// 	</div>
+// 	<div className='w-1/3 border-2 border-white p-3 px-3'>
+// 		<img
+// 		src=${Reviews[i + 1].logo}
+// 			alt='logo'
+// 			className='h-20 opacity-70 my-3'
+// 		/>
+// 		<h2 className='text-white text-xl my-3'>${Reviews[i + 1].name}</h2>
+// 		<p className='text-xm text-white my-3 text-center'>
+// 			${Reviews[i + 1].review}
+// 		</p>
+// 		<h2 className='text-white text-3xl my-3'>${Reviews[i + 1].rating}</h2>
+// 		<div>${Reviews[i + 1].stars}</div>
 
-// </div>`
-//}
+// 	</div>
+// 	<div className='w-1/3 border-2 border-white p-3 px-3'>
+// 		<img
+// 		src=${Reviews[i + 2].logo}
+// 			alt='logo'
+// 			className='h-20 opacity-70 my-3'
+// 		/>
+// 		<h2 className='text-white text-xl my-3'>${Reviews[i + 2].name}</h2>
+// 		<p className='text-xm text-white my-3 text-center'>
+// 			${Reviews[i + 2].review}
+// 		</p>
+// 		<h2 className='text-white text-3xl my-3'>${Reviews[i + 2].rating}</h2>
+// 		<div>${Reviews[i + 2].stars}</div>
+
+// 	</div>
+// 	`;
+// 		formatedReviews.push(groupedReviews);
+// 	}
+// };
+// function convertToPlain(html) {
+// 	// Create a new div element
+// 	var tempDivElement = document.createElement("div");
+
+// 	// Set the HTML content with the given value
+// 	tempDivElement.innerHTML = html;
+
+// 	// Retrieve the text property of the element
+// 	return tempDivElement.textContent || tempDivElement.innerText || "";
+// }
 
 const Home = () => {
+	// useEffect(() => {
+	// 	handleFormatReviews();
+	// }, []);
+
 	return (
 		<div className='flex flex-col mt-[30vh] gap-[30vh]  justify-center'>
 			<a href='https://wa.me/+5492664463274' target={"blank"}>
@@ -124,100 +169,150 @@ const Home = () => {
 					</button>
 				</div>
 			</div>
-			<div className=' flex flex-col items-center bg-bn bg-no-repeat bg-fixed bg-cover w-full h-[600px]'>
-				<div className='w-[70vw] mx-auto my-auto'>
+			<div className=' flex flex-col items-center bg-bn bg-no-repeat bg-fixed bg-cover w-full h-[100vh]'>
+				<div>
+					<h1 className='bg-black/50  p-3 mt-5 text-white text-2xl rounded'>
+						Nuestros huespedes andan diciendo ...
+					</h1>
+				</div>
+				<div className='w-[70vw]  mx-auto my-auto'>
 					<Carousel
-						autoPlay={true}
+						// autoPlay={true}
+
 						showThumbs={false}
 						showIndicators={true}
 						infiniteLoop={true}
 						showStatus={false}>
-						<div className='flex h-[400px] bg-black/50 p-3'>
-							<div className='w-1/3 border-x-2 border-gray p-3 px-3'>
-								<img
-									src={logoBooking}
-									alt='logo'
-									className='h-20 opacity-70 my-3'
-								/>
-								<h2 className='text-white text-xl my-3'>Romina: </h2>
-								<p className='text-xm text-white my-3 text-center'>
-									"Una paz increible y una belleza unica. Si tengo que comparalo
-									con "algo", seria con la receta de la abuela que mas me gusta.
-									Volveria siempre."
-								</p>
-								<h2 className='text-white text-3xl my-3'>10</h2>
+						{/* {formatedReviews.map((el) => {
+							return <div className='flex h-[400px] bg-black/50 p-3'>el</div>;
+						})} */}
+						<div className='h-[60vh] flex bg-black/50 p-5'>
+							<div className='w-1/3  flex flex-col items-center justify-center border-r-2 border-gray'>
+								<div className=' h-1/3 '>
+									<img
+										src={logoBooking}
+										alt='logo'
+										className='h-2/3 opacity-75 '
+									/>
+									<h2 className='h-1/3 text-white text-xl mt-1'>Romina: </h2>
+								</div>
+								<div className='h-[43%] py-3 px-5 '>
+									<p className='text-xm text-white my-3 text-center'>
+										" Una paz increible y una belleza unica. Si tengo que
+										comparalo con "algo", seria con la receta de la abuela que
+										mas me gusta. Volveria siempre. "
+									</p>
+								</div>
+								<div className='h-[23%]  p-3'>
+									<h2 className='text-white text-3xl my-3'>10</h2>
+								</div>
 							</div>
-							<div className='w-1/3 border-x-2 border-gray p-3 px-3'>
-								<img
-									src={logoBooking}
-									alt='logo'
-									className='h-20 opacity-70 my-3'
-								/>
-								<h2 className='text-white text-xl my-3'>Romina: </h2>
-								<p className='text-xm text-white my-3 text-center'>
-									"Una paz increible y una belleza unica. Si tengo que comparalo
-									con "algo", seria con la receta de la abuela que mas me gusta.
-									Volveria siempre."
-								</p>
-								<h2 className='text-white text-3xl my-3'>10</h2>
+							<div className='w-1/3  flex flex-col items-center justify-center border-r-2 border-gray'>
+								<div className='h-1/3 '>
+									<img
+										src={logoBooking}
+										alt='logo'
+										className='h-2/3 opacity-75 '
+									/>
+									<h2 className='text-white h-1/3 text-xl mt-1'>Maria: </h2>
+								</div>
+								<div className='h-[43%]  py-3 px-5'>
+									<p className='text-xm text-white my-3 text-center'>
+										" Impecable y muy cálida la atención de Marcela. Hermosa la
+										propiedad y las vistas de las sierras y del valle. Los
+										chicos disfrutaron muchísimo de la pileta y de la mesa de
+										ping-pong. Muy tranquilo. "
+									</p>
+								</div>
+								<div className='h-[23%] p-3'>
+									<h2 className='text-white text-3xl my-3'>9</h2>
+								</div>
 							</div>
-							<div className='w-1/3 border-x-2 border-gray  p-3 px-3'>
-								<img
-									src={logoBooking}
-									alt='logo'
-									className='h-20 opacity-70 my-3'
-								/>
-								<h2 className='text-white text-xl my-3'>Romina: </h2>
-								<p className='text-xm text-white my-3 text-center'>
-									"Una paz increible y una belleza unica. Si tengo que comparalo
-									con "algo", seria con la receta de la abuela que mas me gusta.
-									Volveria siempre."
-								</p>
-								<h2 className='text-white text-3xl my-3'>10</h2>
+							<div className='w-1/3 flex flex-col items-center justify-center'>
+								<div className='h-1/3 '>
+									<img
+										src={logoBooking}
+										alt='logo'
+										className='h-2/3 opacity-75 '
+									/>
+									<h2 className='text-white h-1/3 mt-1 text-xl'>Ricardo: </h2>
+								</div>
+								<div className='h-[43%] p-3 overflow-auto'>
+									<p className='text-xm py-3 px-5 text-white mt-10 text-center'>
+										" Excelente lugar !!!!! "
+									</p>
+								</div>
+								<div className='h-[23%]  p-3'>
+									<h2 className='text-white text-3xl my-3'>9</h2>
+								</div>
 							</div>
 						</div>
-						<div className='flex h-[400px] bg-black/50 p-3'>
-							<div className='w-1/3 border-x-2 border-grapx-3'>
-								<img
-									src={logoBooking}
-									alt='logo'
-									className='h-20 opacity-70 my-3'
-								/>
-								<h2 className='text-white text-xl my-3'>Romina: </h2>
-								<p className='text-xm text-white my-3 text-center'>
-									"Una paz increible y una belleza unica. Si tengo que comparalo
-									con "algo", seria con la receta de la abuela que mas me gusta.
-									Volveria siempre."
-								</p>
-								<h2 className='text-white text-3xl my-3'>10</h2>
+						<div className=' flex h-[60vh] bg-black/50 p-5'>
+							<div className='w-1/3  flex flex-col items-center justify-center border-r-2 border-gray'>
+								<div className=' h-1/3 '>
+									<img
+										src={logoGoogle}
+										alt='logo'
+										className='h-2/3 opacity-75'
+									/>
+									<h2 className='text-white h-1/3 mt-1 text-xl'>
+										andreagabriela1978:{" "}
+									</h2>
+								</div>
+								<div className='h-[43%] py-3 px-5 overflow-auto'>
+									<p className='text-xm text-white my-3 text-center '>
+										" Un lugar soñado! La atención de Marcela y su familia es
+										espectacular! Muy cordiales y bien predispuestos! Las
+										cabañas son hermosas y súper cómodas, la pileta y el jacuzzi
+										espectaculares!! Para volver a visitar sin dudarlo!!. "
+									</p>
+								</div>
+								<div className='flex items-center h-[23%] w-[45%] p-3'>
+									<img src={estrellas} alt='' />
+								</div>
 							</div>
-							<div className='w-1/3 border-x-2 border-gray p-3 px-3'>
-								<img
-									src={logoBooking}
-									alt='logo'
-									className='h-20 opacity-70 my-3'
-								/>
-								<h2 className='text-white text-xl my-3'>Romina: </h2>
-								<p className='text-xm text-white my-3 text-center'>
-									"Una paz increible y una belleza unica. Si tengo que comparalo
-									con "algo", seria con la receta de la abuela que mas me gusta.
-									Volveria siempre."
-								</p>
-								<h2 className='text-white text-3xl my-3'>10</h2>
+							<div className='w-1/3  flex flex-col items-center justify-center border-r-2 border-gray'>
+								<div className='h-1/3 '>
+									<img
+										src={logoGoogle}
+										alt='logo'
+										className='h-2/3 opacity-75'
+									/>
+									<h2 className='text-white h-1/3 mt-1 text-xl'>
+										Lía Bedatou:{" "}
+									</h2>
+								</div>
+								<div className='h-[43%]  py-3 px-5 overflow-auto'>
+									<p className='text-xm text-white my-3 text-center'>
+										" Hermoso lugar! Súper recomendable! Las cabañas completas y
+										muy cómodas. La atención de Marcela y Laurita: un lujo! Nos
+										atendieron con mucha amabilidad y cariño, incluso a Lupita (
+										nuestra mascota). "
+									</p>
+								</div>
+								<div className='flex items-center h-[23%] w-[45%] p-3'>
+									<img src={estrellas} alt='' />
+								</div>
 							</div>
-							<div className='w-1/3 border-x-2 border-gray p-3 px-3'>
-								<img
-									src={logoBooking}
-									alt='logo'
-									className='h-20 opacity-70 my-3'
-								/>
-								<h2 className='text-white text-xl my-3'>Romina: </h2>
-								<p className='text-xm text-white my-3 text-center'>
-									"Una paz increible y una belleza unica. Si tengo que comparalo
-									con "algo", seria con la receta de la abuela que mas me gusta.
-									Volveria siempre."
-								</p>
-								<h2 className='text-white text-3xl my-3'>10</h2>
+							<div className='w-1/3 flex flex-col items-center justify-center'>
+								<div className='h-1/3 '>
+									<img
+										src={logoGoogle}
+										alt='logo'
+										className='h-2/3 opacity-75'
+									/>
+									<h2 className='text-white h-1/3 mt-1 text-xl'>Ricardo: </h2>
+								</div>
+								<div className='h-[43%] p-3 overflow-auto'>
+									<p className='text-xm py-3 text-white text-center'>
+										" Un lugar encantador, la vista es hermosa, las cabañas un
+										espectáculo, y los dueños excelentes y muy atentos.
+										Totalmente recomendable. "
+									</p>
+								</div>
+								<div className='flex items-center h-[23%] w-[45%] p-3'>
+									<img src={estrellas} alt='' />
+								</div>
 							</div>
 						</div>
 					</Carousel>
