daa4deca85cfe7542639de9644b999957233a800
MartinCanolik <martin.canolik@gmail.com>
2023-03-19 21:35:52 -0300

    sendinblue new api key

--- src/components/booking/Booking.jsx
+++ src/components/booking/Booking.jsx
@@ -29,7 +29,6 @@ const Booking = () => {
 
 	const submitForm = async (values) => {
 		try {
-			console.log("clave ", process.env.REACT_APP_API_KEY);
 			const response = await axios({
 				method: "post",
 				url: "https://api.sendinblue.com/v3/smtp/email",
